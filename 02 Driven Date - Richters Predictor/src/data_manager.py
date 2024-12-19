9530e2d79887df7d26e3842c4ac844398e503d77
irini-git <irina.nikolkina@gmail.com>
2024-12-19 15:38:52 +0200

    Explore data

--- 02 Driven Date - Richters Predictor/src/data_manager.py
+++ 02 Driven Date - Richters Predictor/src/data_manager.py
@@ -9,7 +9,7 @@ FILENAME_TRAIN_LABELS = '../data/Richters_Predictor_Modeling_Earthquake_Damage_-
 
 class EarthquakeData:
     def __init__(self):
-        self.df_train_labels, self.df_train_values, self.df_test_values = self.load_data()
+        self.df_train_labels, self.df_train_values, self.df_test_values, self.df_train = self.load_data()
         self.explore_data()
 
     def load_data(self):
@@ -22,7 +22,9 @@ class EarthquakeData:
         df_train_values = pd.read_csv(FILENAME_TRAIN_VALUES)
         df_test_values = pd.read_csv(FILENAME_TEST_VALUES)
 
-        return df_train_labels, df_train_values, df_test_values
+        df_train = df_train_labels.merge(df_train_values)
+
+        return df_train_labels, df_train_values, df_test_values, df_train
 
     def explore_data(self):
         """
@@ -30,7 +32,13 @@ class EarthquakeData:
         """
 
         # Explore columns, shape
-        for df in [self.df_test_values]:
-            print(df.columns)
-            for c in df.columns:
-                print(df[c].value_counts())
+        print('-'*10)
+        for df in [self.df_train]:
+            # for c in df.columns:
+                # print(df[c].value_counts())
+            print(f'Dataframe shape : {df.shape}')
+            print(f'Null values in database : {df.isnull().sum().sum()}')
+            print(df.info())
+            with pd.option_context('display.max_rows', None, 'display.max_columns', None):
+                print(df.describe(include=object))
+
